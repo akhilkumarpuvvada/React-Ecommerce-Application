4894b7fe145ab092e83578ef981ef0eb605dfdeb
akhilkumarpuvvada <pnvakhil13@gmail.com>
2021-10-16 17:28:09 +0200

    updated code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import logo from '../assets/logo.svg'
+
 import { FaBars } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { links } from '../utils/constants'
@@ -15,9 +15,7 @@ const Nav = () => {
     <NavContainer>
       <div className='nav-center'>
         <div className='nav-header'>
-          <Link to='/'>
-            <img src={logo} alt='comfy sloth' />
-          </Link>
+          
           <button type='button' className='nav-toggle' onClick={openSidebar}>
             <FaBars />
           </button>
